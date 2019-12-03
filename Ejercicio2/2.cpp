57b852accad3c448e5558ccf4ffd2fbbd0fafcf2
nico794 <nico.794@gmail.com>
2019-12-03 13:13:50 -0300

    separacion en hilos

---
+++ Ejercicio2/2.cpp
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#include <list>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+#define MAX_THREADS 50
+
+struct vector{
+    int id;
+    double x;
+    double y;
+};
+list<vector> vectores;
+
+double resultados[MAX_THREADS];
+
+struct vectoresPorHilo{
+    int hilo;
+    list<vector> vectores;
+};
+list<vectoresPorHilo> hilos;
+
+pthread_t tabla_thr[MAX_THREADS];
+
+void* imprime_mensaje(void *ptr) {
+    int i;
+    int id;
+
+    // list<vector>::iterator it = vectores.begin();
+    list<vectoresPorHilo>::iterator it = hilos.begin();
+
+    id = *(int *) ptr;
+    cout<<id<<" "<<hilos.size()<<endl;
+    if(id < hilos.size()){
+        advance(it,id);
+        vectoresPorHilo vph = *it;
+        cout<<"El hilo "<<vph.hilo<<" realizara la suma de "<<vph.vectores.size()<<" vectores."<<endl;
+        list<vector>::iterator itVectores = vph.vectores.begin();
+        while(itVectores != vph.vectores.end())
+        {
+            double res = (itVectores->x)+(itVectores->y);
+            // cout<<(*itVectores).x<<" "<<(*itVectores).y<<endl;
+            // resultados.push_back(res);
+            resultados[itVectores->id] = res;
+            itVectores++;
+        }
+    }
+    else
+        cout<<"El hilo "<<id<<" realizara la suma de "<<0<<" vectores."<<endl;
+
+    return 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+    pthread_t hilo0, hilo1,hilo2;
+    int id0=0, id1=1, id2=2;
+    int cant_threads = atoi(argv[1]);
+
+    pthread_t threads[cant_threads];
+    int cant=0, cantVectores = 0;
+    ifstream archivo;
+    archivo.open("archivo_datos");
+    if (archivo.fail()) {
+        cerr << "Error al abrir el archivo" << endl;
+        archivo.close();
+        exit(1);
+    }
+    int hiloActual = 0;
+    while (!archivo.eof())
+    {
+        // cout<<"------------------------------"<<endl;
+        vector v;
+        string x, y;
+        getline(archivo,x,',');
+        v.x = stod(x);
+        getline(archivo,y,'\n');
+        v.y = stod(y);
+        v.id = cantVectores;
+        bool esta = false;
+        // cout<<"hiloActual "<<hiloActual<<" lista "<<hilos.empty()<<endl;
+        if(hilos.empty()){
+            vectoresPorHilo vph;
+            vph.hilo = hiloActual;
+            vph.vectores.push_back(v);
+            hilos.push_back(vph);
+        }
+        else{
+            list<vectoresPorHilo>::iterator itHilos = hilos.begin();
+            // for (itHilos = hilos.begin(); itHilos != hilos.end(); itHilos++)
+            while(itHilos != hilos.end() && !esta)
+            {
+                // cout<<"itHilos->hilo == hiloActual "<<itHilos->hilo<<" "<<hiloActual<<" "<<(itHilos->hilo == hiloActual)<<endl;
+                if(itHilos->hilo == hiloActual){
+                    itHilos->vectores.push_back(v);
+                    esta = true;
+                }
+                itHilos++;
+            }
+            if(!esta){
+                // cout<<"no esta"<<endl;
+                vectoresPorHilo vph;
+                vph.hilo = hiloActual;
+                vph.vectores.push_back(v);
+                hilos.push_back(vph);
+            }
+        }
+
+        hiloActual++;
+        if(hiloActual >= cant_threads)
+            hiloActual = 0;
+        // if (pthread_create(&tabla_thr[cant],NULL,&imprime_mensaje,(void*)&cant)!= 0){
+		// 	perror("pthread_create");
+		// 	exit(1);
+		// }
+        // pthread_join(tabla_thr[cant],NULL);
+        // cant++;
+    // cout<<"hilos.size()"<<hilos.size()<<endl;
+    //     cout<<"------------------------------"<<endl;
+        cantVectores++;
+    }
+    hilos.begin();
+    cout<<"hilos.size()"<<hilos.size()<<endl;
+    // cout<<"cant vectores "<<vectores.size()<<endl;
+    // list<vectoresPorHilo>::iterator itHilos;
+    // for (itHilos = hilos.begin(); itHilos != hilos.end(); itHilos++)
+    // {
+    //     cout<<"hilo "<<itHilos->hilo<<endl;
+    //     cout<<"cant vectores que procesa "<<itHilos->vectores.size()<<endl;        
+    // }
+
+    while(cant < cant_threads){
+        if (pthread_create(&tabla_thr[cant],NULL,&imprime_mensaje,(void*)&cant)!= 0){
+            perror("pthread_create");
+            exit(1);
+        }
+        pthread_join(tabla_thr[cant],NULL);
+        cant++;
+    }
+    ofstream salida("salida");
+    for(int i=0;i<cantVectores;i++)
+    {
+        salida<<resultados[i]<<endl;
+    }
+    return 0;
+}
